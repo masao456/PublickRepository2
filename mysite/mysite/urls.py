38828c5d408bc583b282ee64dcf5d5bebeca64eb
masao456 <roadstar.nogi@gmail.com>
2024-07-17 00:51:59 +0900

    “初めてのコミット”

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myapp/', include('myapp.urls')),
+    path('', RedirectView.as_view(url='/myapp/home/', permanent=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
